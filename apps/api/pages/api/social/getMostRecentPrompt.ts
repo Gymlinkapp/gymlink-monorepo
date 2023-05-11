b5d7d511c417c0aaad342e5fbb2e20b9cc067b75
Dylan <dylan.reeder5@gmail.com>
2023-05-11 10:19:43 -0400

    update explore middleware for clerk & cors headers for prompt

--- apps/api/pages/api/social/getMostRecentPrompt.ts
+++ apps/api/pages/api/social/getMostRecentPrompt.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-import prisma from "@/lib/prisma";
-import { GenericData } from "@/types/GenericData";
+import prisma from '@/lib/prisma';
+import { GenericData } from '@/types/GenericData';
 
 type Data = {} & GenericData;
 
@@ -9,17 +9,34 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Handle the OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   try {
-    // get most recent prompt 
+    // get most recent prompt
     const prompt = await prisma.prompt.findFirst({
       orderBy: {
-        createdAt: "desc",
+        createdAt: 'desc',
       },
     });
 
     res.status(200).json({ prompt });
   } catch (error) {
     console.log(error);
-    throw new Error("error");
+    throw new Error('error');
   }
 }
