a17da7d65c921c8a37fb9299d6da48ae97eb38e4
Dylan <dylan.reeder5@gmail.com>
2023-05-11 17:10:13 -0400

    if a user already has answered, they can't answer again

--- apps/api/pages/api/social/answerPrompt.ts
+++ apps/api/pages/api/social/answerPrompt.ts
@@ -17,35 +17,52 @@ export default async function handler(
   res: NextApiResponse
 ) {
   // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   // Handle the OPTIONS request
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
   }
   const input = req.body as Input;
+  if (input.answer.length === 0) {
+    return res.status(400).json({ error: 'Answer cannot be empty' });
+  }
   console.log(input);
   try {
     let user;
     if (input.userId) {
-    user = await prisma.user.findUnique({
-      where: {
-        id: input.userId,
-      },
-    });
+      user = await prisma.user.findUnique({
+        where: {
+          id: input.userId,
+        },
+        include: {
+          userPrompts: true,
+        },
+      });
     } else if (input.email) {
       user = await prisma.user.findUnique({
         where: {
           email: input.email,
         },
+        include: {
+          userPrompts: true,
+        },
       });
     }
 
     if (!user) throw new Error('User not found');
+    if (user.userPrompts.find((prompt) => prompt.promptId === input.promptId))
+      throw new Error('User already answered this prompt');
 
     const prompt = await prisma.prompt.findUnique({
       where: {
