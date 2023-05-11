a3dad41ff2e282729917b751b1f7ef45fa6e476f
Dylan <dylan.reeder5@gmail.com>
2023-05-11 17:09:27 -0400

    safer syncing of prompts to most recent

--- apps/explore/utils/syncDailyPrompt.ts
+++ apps/explore/utils/syncDailyPrompt.ts
@@ -1,6 +1,9 @@
-import { UserPrompt } from "@/hooks/useGetUserByEmail";
+import { UserPrompt } from '@/hooks/useGetUserByEmail';
 
-export default function syncDailyPrompt(promptId: string, userPrompts: UserPrompt[]) {
-  const prompt = userPrompts.find((prompt) => prompt.promptId === promptId);
-  return prompt;
+export default function syncDailyPrompt(
+  promptId: string,
+  userPrompts: UserPrompt[]
+) {
+  if (!userPrompts) return null;
+  return userPrompts.find((prompt) => prompt.promptId === promptId);
 }
